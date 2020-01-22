225a313efffc23b1bfdc34bf5c55c39855bb2780
unknown <nduross1@gmail.com>
2020-01-22 05:56:08 -0800

    first commit

---
+++ movie/src/helpers/getApi.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from 'react';
+import {StyleSheet} from 'react-native';
+
+import movies from '../api/movies';
+
+export const getApi = async (movie, setMovie, mount) => {
+  mount = true;
+
+  if (mount) {
+    try {
+      const response = await movies.get('/550', {
+        params: {
+          api_key: '712ad7462949bf536a1eea5dd0a07c0f',
+        },
+      });
+      await setMovie(response.data);
+      console.log(movie);
+    } catch (error) {
+      if (error) setMovie('Please try again later');
+    }
+  }
+  mount = false;
+};
